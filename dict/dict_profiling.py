3f65ed898dc0b854c838d36c34aa84dc72d3de67
Damian Nowok <damian.nowok@gmail.com>
2014-09-14 19:21:00 +0200

    Fixing small problem with parsing

--- dict/dict_profiling.py
+++ dict/dict_profiling.py
@@ -6,7 +6,7 @@ import pstats
 import sys
 
 if __name__ == "__main__":
-    test_range = 10000
+    test_range = 1000
 
     init = ""
     for i in range(test_range):
@@ -21,20 +21,20 @@ if __name__ == "__main__":
         cmd += "a" + str(i) + "[r_str]=r\n"
 
     p = cProfile.Profile()
-    s = StringIO.StringIO()
     result = []
     print ""
-    for i in range(10000):
+    for i in range(1000):
+        s = StringIO.StringIO()
         p.run(cmd)
         ps = pstats.Stats(p, stream=s)
         ps.print_stats()
         result.append(float(s.getvalue().split("\n")[0].strip().split(" ")[4]))
         sys.stdout.write(".")
+        sys.stdout.flush()
 
     print ""
-    avg = sum(result)/len(result) + 0.00001
-    print "Avrage: " + str(avg)
 
     for i,n in enumerate(result):
-        if n > avg:
-            print str(i) + " : " + str(n)
+        if i > 0 and i < len(result) - 2:
+            if result[i - 1] < result[i] and result[i + 1] < result[i]:
+                print str(i) + " : " + str(n)
