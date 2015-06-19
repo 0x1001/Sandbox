98bfcd42df6c0cea872cb0c81b7b8e598a9ab82a
Damian Nowok <damian.nowok@gmail.com>
2015-06-19 19:52:17 +0200

    Largest sum kata

--- kata/largest_sum/python/largest_sum.py
+++ kata/largest_sum/python/largest_sum.py
@@ -1,35 +1,26 @@
 import unittest
 
+
 def largest_sum(A):
-    if A == []:
+    return _largest_sum(A, 0, len(A))
+
+
+def _largest_sum(A, start, stop):
+
+    if len(A[start:stop]) == 0:
         return 0
 
-    maxsum = 0
-    sum = 0
+    if len(A[start:stop]) == 1:
+        return A[start]
 
-    for i in range(len(A)):
-        sum += A[i]
+    max_1 = A[start]
+    max_2 = _largest_sum(A, start + 1, stop)
+    max_3 = A[stop - 1]
+    max_4 = _largest_sum(A, start, stop - 1)
+    max_5 = sum(A[start:stop])
 
-        if sum > maxsum:
-            maxsum = sum
-        elif sum < 0:
-            sum = 0
+    return max(max_1, max_2, max_3, max_4, max_5)
 
-    return maxsum
-
-def largest_sum(A):
-    maxsum = None
-    for i in range(len(A)):
-        sum = 0
-        for j in range(i, len(A)):
-            sum += A[j]
-
-        if maxsum is None:
-            maxsum = None
-        if maxsum < sum:
-            maxsum = sum
-
-    return maxsum if maxsum is not None else 0
 
 class Test_largest_sum(unittest.TestCase):
     def test_base_case(self):
@@ -40,9 +31,16 @@ class Test_largest_sum(unittest.TestCase):
 
     def test_negative(self):
         self.assertEqual(largest_sum([-1]), -1)
+        self.assertEqual(largest_sum([-100, -1, -1, -3, -5, -100]), -1)
 
     def test_5_numbers(self):
         self.assertEqual(largest_sum([5, -9, 6, -2, 3]), 7)
+        self.assertEqual(largest_sum([-100, 10, 10, -3, -5]), 20)
+
+    def test_large_num(self):
+        self.assertEqual(largest_sum([100, -1, -1, -3, -5, 100]), 190)
+        self.assertEqual(largest_sum([-100, 10, -1, -3, -5, -100]), 10)
+
 
 if __name__ == "__main__":
     unittest.main()
