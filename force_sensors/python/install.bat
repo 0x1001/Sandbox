0d84ddf53c295045879bd8903500c99eb4abfb5c
0x1001 <damian.nowok@gmail.com>
2018-06-06 00:07:51 +0200

    Added installation script

---
+++ force_sensors/python/install.bat
@@ -0,0 +1,18 @@
+PYTHON=c:\tools\Python36\python.exe
+
+IF EXIST ".venv" (
+    rmdir /q /s .venv
+)
+
+%PYTHON% -m venv .venv
+
+IF NOT EXIST ".venv" (
+	echo Python virtual environment faild to create.
+	pause
+	exit 1
+)
+
+call .venv\scripts\activate.bat
+
+pip install pyaudio
+pip install pyserial
