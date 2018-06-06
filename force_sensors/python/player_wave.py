8fa6c57292d75dc6e93d4ac4e476f5563095a615
0x1001 <damian.nowok@gmail.com>
2018-06-07 00:40:56 +0200

    Added second sound to stream

--- force_sensors/python/player_wave.py
+++ force_sensors/python/player_wave.py
@@ -4,25 +4,34 @@ import serial
 import common
 import threading
 
-CHUNK = 256
+CHUNK = 32
 
 
 class Volume:
     def __init__(self):
-        self._volume = 0
+        self._volume_1 = 0
+        self._volume_2 = 0
         self._lock = threading.RLock()
         threading.Thread(target=self._volume_control).start()
 
-    def set_volume(self, v):
+    def set_volume_1(self, v):
         with self._lock:
-            self._volume = v
+            self._volume_1 = v
 
-    def get_volume(self):
+    def set_volume_2(self, v):
         with self._lock:
-            return self._volume
+            self._volume_2 = v
+
+    def get_volume_1(self):
+        with self._lock:
+            return self._volume_1
+
+    def get_volume_2(self):
+        with self._lock:
+            return self._volume_2
 
     def _volume_control(self):
-        ser = serial.Serial("COM34", 9600)
+        ser = serial.Serial("COM3", 9600)
         common.wait_for_start(ser)
 
         while True:
@@ -30,48 +39,100 @@ class Volume:
 
             if common.first_button(line):
                 potentiometer = common.get_value(line)
-                if 0 < potentiometer < 200:
-                    self.set_volume(0)
-                elif 200 < potentiometer < 400:
-                    self.set_volume(0.1)
-                elif 400 < potentiometer < 700:
-                    self.set_volume(0.25)
-                elif 700 < potentiometer < 1024:
-                    self.set_volume(1)
+                volume = self._calculate_volume_1(potentiometer)
+                self.set_volume_1(volume)
+
+            elif common.second_button(line):
+                potentiometer = common.get_value(line)
+                volume = self._calculate_volume_2(potentiometer)
+                self.set_volume_2(volume)
+
+    def _calculate_volume_1(self, potentiometer):
+        if 0 < potentiometer <= 700:
+            volume = 0
+        elif 700 < potentiometer <= 800:
+            volume = 0.1
+        elif 800 < potentiometer <= 900:
+            volume = 0.5
+        elif 900 < potentiometer <= 1024:
+            volume = 1
+        else:
+            volume = 0
+
+        return volume
+
+    def _calculate_volume_2(self, potentiometer):
+        if 0 < potentiometer <= 500:
+            volume = 0
+        elif 500 < potentiometer <= 600:
+            volume = 0.1
+        elif 600 < potentiometer <= 700:
+            volume = 0.5
+        elif 700 < potentiometer <= 1024:
+            volume = 1
+        else:
+            volume = 0
+
+        return volume
 
 
 def play():
-    f = wave.open(r"samples\EpicScifi_Mini_SP\ButtonFX_03_554.wav","rb")
+    file_1 = wave.open(r"samples\051-d#.wav","rb")
+    file_2 = wave.open(r"samples\048-c.wav", "rb")
     p = pyaudio.PyAudio()
 
-    print("Sample width: {0}".format(f.getsampwidth()))
-    print("Channels width: {0}".format(f.getnchannels()))
+    print("Sample width 1: {0}".format(file_1.getsampwidth()))
+    print("Channels width 1: {0}".format(file_1.getnchannels()))
 
-    stream = p.open(format=p.get_format_from_width(f.getsampwidth()),
-                    channels=f.getnchannels(),
-                    rate=f.getframerate(),
+    print("Sample width 2: {0}".format(file_2.getsampwidth()))
+    print("Channels width 2: {0}".format(file_2.getnchannels()))
+
+    if file_1.getsampwidth() != file_2.getsampwidth() or file_1.getnchannels() != file_2.getnchannels():
+        raise Exception("Sample size or number of channels does not match!")
+
+    stream = p.open(format=p.get_format_from_width(file_1.getsampwidth()),
+                    channels=file_1.getnchannels(),
+                    rate=file_1.getframerate(),
                     output=True)
 
     vol = Volume()
     while True:
-        data = f.readframes(CHUNK)
+        data_1 = file_1.readframes(CHUNK)
+        data_2 = file_2.readframes(CHUNK)
 
-        if not data:
-            f.rewind()
-            data = f.readframes(CHUNK)
+        if not data_1:
+            file_1.rewind()
+            data_1 = file_1.readframes(CHUNK)
 
-        modified_data = b''
-        volume = vol.get_volume()
-        for i in range(0, len(data), 6):
-            ch1 = int.from_bytes(data[i:i+3], "little", signed=True)
-            ch2 = int.from_bytes(data[i+3:i+6], "little", signed=True)
+        if not data_2:
+            file_2.rewind()
+            data_2 = file_2.readframes(CHUNK)
 
-            ch1 = int(ch1 * volume)
-            ch2 = int(ch2 * volume)
+        output = b''
+        volume_1 = vol.get_volume_1()
+        volume_2 = vol.get_volume_2()
+        for i in range(0, len(data_1), 6):
+            # First sample
+            s1_ch1 = int.from_bytes(data_1[i:i+3], "little", signed=True)
+            s1_ch2 = int.from_bytes(data_1[i+3:i+6], "little", signed=True)
 
-            modified_data = modified_data + ch1.to_bytes(3, "little", signed=True) + ch2.to_bytes(3, "little", signed=True)
+            s1_ch1 = int(s1_ch1 * volume_1)
+            s1_ch2 = int(s1_ch2 * volume_1)
 
-        stream.write(modified_data)
+            # Second sample
+            s2_ch1 = int.from_bytes(data_2[i:i+3], "little", signed=True)
+            s2_ch2 = int.from_bytes(data_2[i+3:i+6], "little", signed=True)
+
+            s2_ch1 = int(s2_ch1 * volume_2)
+            s2_ch2 = int(s2_ch2 * volume_2)
+
+            # Combined sound:
+            ch1 = int((s1_ch1 + s2_ch1) / 2)
+            ch2 = int((s1_ch2 + s2_ch2) / 2)
+
+            output = output + ch1.to_bytes(3, "little", signed=True) + ch2.to_bytes(3, "little", signed=True)
+
+        stream.write(output)
 
     stream.stop_stream()
     stream.close()
